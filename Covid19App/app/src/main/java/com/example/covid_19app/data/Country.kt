d21f8502e5930d6c847f86d382bb76b6f715870a
abaialihan <abai.alihan@gmail.com>
2021-05-07 23:36:13 +0600

    tied imageViewFlag and spinner

---
+++ Covid19App/app/src/main/java/com/example/covid_19app/data/Country.kt
@@ -0,0 +1,7 @@
+package com.example.covid_19app.data
+
+import com.example.covid_19app.R
+
+public class Country {
+
+}
--- Covid19App/app/src/main/java/com/example/covid_19app/fragments/StaticFragment.kt
+++ Covid19App/app/src/main/java/com/example/covid_19app/fragments/StaticFragment.kt
@@ -8,11 +8,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.Spinner
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.example.covid_19app.R
+import com.example.covid_19app.data.Country
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,6 +28,12 @@ class StaticFragment : Fragment() {
     private var param2: String? = null
 
     private lateinit var spinnerCountry: Spinner
+    private lateinit var flagImageView: ImageView
+
+    //лист из названий стран которые мы выбраем в  спиннере
+    val listOfCountry: Array<String> = arrayOf("KGZ", "USA", "RUS", "CND")
+    // id иконок стран собраем  массив чтобы связать с позициями списка стран
+    val flagPosition: Array<Int> = arrayOf(R.drawable.ic_kgz, R.drawable.ic_usa, R.drawable.ic_rus, R.drawable.ic_cnd)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,17 +48,22 @@ class StaticFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_static, container, false)
+        val rootView: View = inflater.inflate(R.layout.fragment_static, container, false)
+        spinnerCountry = rootView.findViewById(R.id.spinner_country)
+        flagImageView = rootView.findViewById(R.id.flagImageView)
         itemSelect()
+        return rootView
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        spinnerCountry = view.findViewById(R.id.spinner_country)
-    }
-
-
+    // здесь обрабатываем выбранный item из спиннера
     private fun itemSelect(){
+        //создвем адаптер для спинера
+        val adapter = activity?.let { ArrayAdapter(it, android.R.layout.simple_spinner_dropdown_item, listOfCountry) }
+        adapter?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // ставим созданный адаптер к спиннеру
+        spinnerCountry!!.adapter = adapter
+
+        // ItemSelectedListener для спинера
         spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
@@ -62,6 +71,8 @@ class StaticFragment : Fragment() {
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 Toast.makeText(activity, "You selected ${parent?.getItemAtPosition(position).toString()}", Toast.LENGTH_LONG).show()
+                //меняем картину в flagImageView по выбору item в спинере
+                flagImageView.setImageResource(flagPosition[spinnerCountry.selectedItemPosition])
             }
         }
     }
